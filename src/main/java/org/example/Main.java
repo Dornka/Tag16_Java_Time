627a385c28e9b39cdeef64789eb8cd7be01327ff
FIRST_NAME LAST_NAME <Skiber03@gmail.com>
2023-09-11 15:55:55 +0200

    added currentDate, futureDate and method to compare them

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
-
+import java.time.LocalDateTime;
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LocalDateTime actualDate = LocalDateTime.now();
+
+        System.out.println("Aktuelles Datum: " + actualDate);
+
+        System.out.println("Aktuelles Datum in zwei Wochen: " +actualDate.plusWeeks(2));
+
+        LocalDate currentDate = LocalDate.now();
+        LocalDate futureDate = LocalDate.of(2023, 9, 29);
+
+        if (currentDate.isBefore(futureDate)){
+            System.out.println("Das aktuelle Datum liegt vor dem festgelegten Datum.");
+        }else if (currentDate.isAfter(futureDate)){
+            System.out.println("Das aktuelle Datum liegt nach dem festgelegten Datum.");
+        }else {
+            System.out.println("Das aktuelle Datum ist gleich dem festgelegten Datum.");
+        }
     }
 }
